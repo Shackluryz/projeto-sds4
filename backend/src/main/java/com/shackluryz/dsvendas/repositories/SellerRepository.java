14a67ebec8464d253412ef980603ef67dabff1e8
Lucas Rodrigues <43302793+Shackluryz@users.noreply.github.com>
2021-09-12 00:05:12 -0300

    [ ADD ] Layers

---
+++ backend/src/main/java/com/shackluryz/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.shackluryz.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.shackluryz.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
